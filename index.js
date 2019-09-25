13378b874f39816050e2a6b79c86c92830919849
sasha <propokerstar90@gmail.com>
2019-09-25 18:27:22 +0300

    Get job page

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cheerio = require('cheerio');
 const url = 'https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof';
 
 const scrapeResults = [];
-async function scrapeCraigList() {
+async function scrapeJobHeader() {
   try{
     const htmlResult = await request.get(url);
     const $ = await cheerio.load(htmlResult);
@@ -15,14 +15,38 @@ async function scrapeCraigList() {
       const title = resultTitle.text();
       const url = resultTitle.attr('href');
       const datePosted = new Date($(element).children('time').attr('datetime'));
-      const scrapeResult = { title, url, datePosted };
+      const hood = $(element).find('.result-hood').text();
+      const scrapeResult = { title, url, datePosted, hood };
       scrapeResults.push(scrapeResult);
     });
 
-    console.log(scrapeResults);
+    return scrapeResults;
   } catch (err) {
     console.error(err)
   }
+}
+
+async function scrapeDescription(jobsWithHeaders){
+  return await Promise.all(jobsWithHeaders.map(async(job) => {
+    try {
+      const htmlResult = await request.get(job.url);
+      const $ = await cheerio.load(htmlResult);
+      $('.print-qrcode-container').remove();
+      job.description = $('#postingbody').text();
+      job.address = $('div.mapaddress').text();
+      const compensationText = $('.attrgroup').children().first().text();
+      job.compensation = compensationText.replace('compensation: ', '');
+      return job;
+    } catch (e) {
+      console.error(e);
+    }
+  }))
+}
+
+async function scrapeCraigList() {
+  const jobsWithHeaders = await scrapeJobHeader();
+  const jobsFullData = await scrapeDescription(jobsWithHeaders);
+  console.log(jobsFullData);
 }
 
 scrapeCraigList();
